2ef44f2c9c8bbd37c9a9a3ad21f9fc7d8d074370
Egor Gluhih <eagluhih@gmail.com>
2023-10-27 01:48:12 +0500

    feat: added wezterm config

---
+++ dot_wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require 'wezterm'
+
+return {
+    font = wezterm.font 'JetBrainsMono Nerd Font',
+    font_size = 15.0,
+    color_scheme = "Grape",
+    enable_scroll_bar = true,
+    keys = {
+        -- This will create a new horizontal split
+        {
+          key = 'v',
+          mods = 'CTRL|SHIFT',
+          action = wezterm.action.SplitHorizontal {
+            domain = 'CurrentPaneDomain'
+          },
+        },
+        -- This will craete a new vertical split
+        {
+            key = 'h',
+            mods = 'CTRL|SHIFT',
+            action = wezterm.action.SplitVertical {
+                domain = 'CurrentPaneDomain'
+            }
+        }
+      },
+}
