2aaf30bbb8b303431b5dcf249338958a6244a970
Egor Gluhih <eagluhih@gmail.com>
2023-10-27 11:33:33 +0500

    feat: added nvim-tree open on start

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,7 +1,22 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+-- Opening tree at startup
+autocmd("VimEnter", {
+  callback = function(data)
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+      return
+    end
+
+    vim.cmd.cd(data.file)
+
+    require("nvim-tree.api").tree.open()
+  end,
+})
