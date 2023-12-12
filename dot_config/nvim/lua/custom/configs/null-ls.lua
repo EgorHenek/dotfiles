475cb74e74af4ecbe655b3c81d5ea1e4898d0f45
Egor Gluhih <eagluhih@gmail.com>
2023-12-12 18:50:56 +0500

    added yaml config

--- dot_config/nvim/lua/custom/configs/null-ls.lua
+++ dot_config/nvim/lua/custom/configs/null-ls.lua
@@ -1,29 +1,33 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-  b.formatting.rustywind,
+	-- webdev stuff
+	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
+	b.formatting.rustywind,
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 
-  -- golang
-  b.formatting.gofumpt,
-  b.formatting.goimports_reviser,
-  b.formatting.golines,
+	-- golang
+	b.formatting.gofumpt,
+	b.formatting.goimports_reviser,
+	b.formatting.golines,
 
-  -- python
-  b.formatting.ruff,
-  b.formatting.blackd,
+	-- python
+	b.formatting.ruff,
+	b.formatting.blackd,
+
+	-- yaml
+	b.diagnostics.yamllint,
+	b.formatting.yamlfmt,
 }
 
 -- local gotest = require("go.null_ls").gotest()
@@ -34,22 +38,22 @@ local sources = {
 -- table.insert(sources, golangci_lint)
 -- table.insert(sources, gotest_codeaction)
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-  on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds {
-        group = augroup,
-        buffer = bufnr,
-      }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format { bufnr = bufnr }
-        end,
-      })
-    end
-  end,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({
+				group = augroup,
+				buffer = bufnr,
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
--- dot_config/nvim/lua/custom/configs/overrides.lua
+++ dot_config/nvim/lua/custom/configs/overrides.lua
@@ -1,101 +1,107 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "sql",
-    "go",
-    "gosum",
-    "gomod",
-    "python",
-  },
-  indent = {
-    enable = true,
-    disable = {
-      "python",
-    },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"sql",
+		"go",
+		"gosum",
+		"gomod",
+		"python",
+		"yaml",
+	},
+	indent = {
+		enable = true,
+		disable = {
+			"python",
+		},
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-    "tailwindcss-language-server",
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"deno",
+		"prettier",
+		"tailwindcss-language-server",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
 
-    -- golang stuff
-    "golangci-lint-langserver",
-    "gopls",
+		-- golang stuff
+		"golangci-lint-langserver",
+		"gopls",
 
-    -- python stuff
-    "ruff",
-    "ruff-lsp",
-    "blackd-client",
-    "pyright",
-  },
+		-- python stuff
+		"ruff",
+		"ruff-lsp",
+		"blackd-client",
+		"pyright",
+
+		-- yaml stuff
+		"yaml-language-server",
+		"yamllint",
+		"yamlfmt",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 M.nvterm = {
-  terminals = {
-    shell = vim.o.shell,
-    list = {},
-    type_opts = {
-      float = {
-        relative = "editor",
-        row = 0.1,
-        col = 0.1,
-        width = 0.8,
-        height = 0.7,
-        border = "single",
-      },
-      horizontal = { location = "rightbelow", split_ratio = 0.3 },
-      vertical = { location = "rightbelow", split_ratio = 0.5 },
-    },
-  },
-  behavior = {
-    autoclose_on_quit = {
-      enabled = true,
-      confirm = true,
-    },
-    close_on_exit = true,
-    auto_insert = true,
-  },
+	terminals = {
+		shell = vim.o.shell,
+		list = {},
+		type_opts = {
+			float = {
+				relative = "editor",
+				row = 0.1,
+				col = 0.1,
+				width = 0.8,
+				height = 0.7,
+				border = "single",
+			},
+			horizontal = { location = "rightbelow", split_ratio = 0.3 },
+			vertical = { location = "rightbelow", split_ratio = 0.5 },
+		},
+	},
+	behavior = {
+		autoclose_on_quit = {
+			enabled = true,
+			confirm = true,
+		},
+		close_on_exit = true,
+		auto_insert = true,
+	},
 }
 return M
