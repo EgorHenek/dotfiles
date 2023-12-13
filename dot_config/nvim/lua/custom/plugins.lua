f8e86b64c63ae5a828a3e48cb218d4896e277387
Egor Gluhih <eagluhih@gmail.com>
2023-12-13 16:43:34 +0500

    feat(nvim): added git-conflict plugin

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -1,163 +1,166 @@
-local overrides = require "custom.configs.overrides"
+local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-    -- config = function()
-    --   require("nvim-treesitter.parsers").get_parser_configs().just = {
-    --   install_info = {
-    --     url = "https://github.com/IndianBoy42/tree-sitter-just",
-    --     files = { "src/parser.c", "src/scanner.cc" },
-    --     branch = "main",
-    --   },
-    --   maintainers = { "@IndianBoy42" },
-    -- }
-    --
-    --     require("nvim-treesitter.install").compilers = { "gcc-11" }
-    --   end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+		-- config = function()
+		--   require("nvim-treesitter.parsers").get_parser_configs().just = {
+		--   install_info = {
+		--     url = "https://github.com/IndianBoy42/tree-sitter-just",
+		--     files = { "src/parser.c", "src/scanner.cc" },
+		--     branch = "main",
+		--   },
+		--   maintainers = { "@IndianBoy42" },
+		-- }
+		--
+		--     require("nvim-treesitter.install").compilers = { "gcc-11" }
+		--   end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  {
-    "andythigpen/nvim-coverage",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("coverage").setup {
-        commands = true,
-        highlights = {
-          covered = { fg = "#C3E88D" },
-          uncovered = { fg = "#F07178" },
-        },
-        signs = {
-          covered = { hl = "CoverageCovered", text = "▎" },
-          uncovered = { hl = "CoverageUncovered", text = "▎" },
-        },
-      }
-    end,
-  },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/neotest-go",
-      "nvim-neotest/neotest-plenary",
-    },
-    config = function()
-      local neotest_ns = vim.api.nvim_create_namespace "neotest"
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
-      require("neotest").setup {
-        adapters = {
-          require "neotest-go" {
-            experimental = {
-              test_table = true,
-            },
-          },
-          require "neotest-plenary",
-        },
-        output = {
-          enabled = true,
-          open_on_run = true,
-        },
-      }
-    end,
-  },
-  {
-    "NvChad/nvterm",
-    opts = overrides.nvterm,
-  },
-  {
-    "Exafunction/codeium.vim",
-    event = "BufEnter",
-    config = function()
-      vim.g.codeium_no_map_tab = 1
-      vim.keymap.set("i", "<c-g>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true, silent = true, desc = "Codeium accept" })
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
-  },
-  {
-    "ray-x/go.nvim",
-    dependencies = {
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-  },
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	{
+		"andythigpen/nvim-coverage",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("coverage").setup({
+				commands = true,
+				highlights = {
+					covered = { fg = "#C3E88D" },
+					uncovered = { fg = "#F07178" },
+				},
+				signs = {
+					covered = { hl = "CoverageCovered", text = "▎" },
+					uncovered = { hl = "CoverageUncovered", text = "▎" },
+				},
+			})
+		end,
+	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/neotest-go",
+			"nvim-neotest/neotest-plenary",
+		},
+		config = function()
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+			require("neotest").setup({
+				adapters = {
+					require("neotest-go")({
+						experimental = {
+							test_table = true,
+						},
+					}),
+					require("neotest-plenary"),
+				},
+				output = {
+					enabled = true,
+					open_on_run = true,
+				},
+			})
+		end,
+	},
+	{
+		"NvChad/nvterm",
+		opts = overrides.nvterm,
+	},
+	{
+		"Exafunction/codeium.vim",
+		event = "BufEnter",
+		config = function()
+			vim.g.codeium_no_map_tab = 1
+			vim.keymap.set("i", "<c-g>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true, silent = true, desc = "Codeium accept" })
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+	},
+	{
+		"ray-x/go.nvim",
+		dependencies = {
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+	},
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	{ "akinsho/git-conflict.nvim", version = "v1.2.2", config = true },
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
+
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
