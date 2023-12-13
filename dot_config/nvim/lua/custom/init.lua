f498ec972559ff51274d9edda67c5c4e50ff8a46
Egor Gluhih <eagluhih@gmail.com>
2023-12-13 16:19:34 +0500

    feat(nvim): added relative numbers

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,22 +1,28 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+	pattern = "*",
+	command = "tabdo wincmd =",
+})
+
+-- relative numbers
+autocmd("BufEnter", {
+	pattern = "*",
+	command = "set relativenumber",
+})
 
 -- Opening tree at startup
 autocmd("VimEnter", {
-  callback = function(data)
-    local directory = vim.fn.isdirectory(data.file) == 1
+	callback = function(data)
+		local directory = vim.fn.isdirectory(data.file) == 1
 
-    if not directory then
-      return
-    end
+		if not directory then
+			return
+		end
 
-    vim.cmd.cd(data.file)
+		vim.cmd.cd(data.file)
 
-    require("nvim-tree.api").tree.open()
-  end,
+		require("nvim-tree.api").tree.open()
+	end,
 })
