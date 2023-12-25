447fd8a725b908c436ac19c1138fd21afafb7fb4
Egor Gluhih <eagluhih@gmail.com>
2023-12-25 22:14:07 +0500

    feat(nvim): added null-ls config

--- dot_config/nvim/lua/plugins/go.lua
+++ dot_config/nvim/lua/plugins/go.lua
@@ -9,11 +9,21 @@ return {
     },
   },
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        go = { "goimports-reviser", "gofumpt" },
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "goimports-reviser" })
+        end,
       },
     },
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.goimports_reviser,
+      })
+    end,
   },
 }
