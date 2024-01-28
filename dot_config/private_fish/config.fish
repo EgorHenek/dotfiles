a803d035ffdc8f37900ac1154ca32f1351210704
Egor Gluhih <eagluhih@gmail.com>
2024-01-28 22:51:50 +0500

    fish: added vim keymode

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,11 @@ set -g theme_date_format "+%a %H:%M"
 set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
 set -g theme_newline_cursor yes
+
+if status is-interactive
+    fish_config theme choose lava
+    fish_vi_key_bindings on
+end
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
