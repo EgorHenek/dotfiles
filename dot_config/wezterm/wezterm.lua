f18a2ec986b0a8fad78920b473a67deadf0861df
Egor Gluhih <eagluhih@gmail.com>
2024-03-05 01:37:47 +0500

    wezterm: added autoconnect to local multiplexer

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,18 +1,18 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font("Iosevka Nerd Font")
+config.font = wezterm.font 'Iosevka Nerd Font'
 config.font_size = 15.0
 
-config.color_scheme = "One Dark (Gogh)"
+config.color_scheme = 'Kanagawa (Gogh)'
 
 config.enable_scroll_bar = true
 
-config.window_background_image = wezterm.config_dir .. "/homa.jpeg"
-config.window_background_image_hsb = {
-	brightness = 0.05,
-	hue = 1.0,
-	saturation = 1.0,
+config.unix_domains = {
+  {
+    name = 'unix',
+  },
 }
+config.default_gui_startup_args = { 'connect', 'unix' }
 
 return config
