1e68ee0f85cfb89895ab3ceec72d20207cfbcaee
Egor Gluhih <eagluhih@gmail.com>
2024-03-09 22:14:48 +0500

    omp: added oh my posh

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,15 +1,9 @@
-set -g theme_display_k8s_context yes
-set -g theme_date_format "+%a %H:%M"
-set -g theme_nerd_fonts yes
-set -g theme_show_exit_status yes
-set -g theme_newline_cursor yes
-
 if status is-interactive
     fish_config theme choose lava
-    fish_vi_key_bindings on
 end
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 zoxide init fish | source
+oh-my-posh init fish --config ~/.config/omp/atomic.json | source
