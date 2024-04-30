a6c2cb94501db7a32d3f28883dc606a4fac0f9cc
Egor Gluhih <eagluhih@gmail.com>
2024-04-30 18:12:58 +0500

    fish: update config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,3 +7,6 @@ end
 
 zoxide init fish | source
 oh-my-posh init fish --config ~/.config/omp/atomic.json | source
+
+fish_add_path -a /Users/henek/.config/.foundry/bin
+source ~/.config/fish/functions/yazi.fish
---
+++ dot_config/private_fish/functions/posix-source.fish
@@ -0,0 +1,7 @@
+function posix-source
+	for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
+    set item (string split -m 1 '=' $line)
+    set -gx $item[1] $item[2]
+    echo "Exported key $item[1]"
+  end
+end
